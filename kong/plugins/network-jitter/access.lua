3a9f89182a0aefbd8129d8f1530a8450d2c506c4
alex1986 <alex19861108@live.cn>
2018-04-08 17:33:09 +0800

    add module: network-jitter

--- kong/plugins/network-jitter/access.lua
+++ kong/plugins/network-jitter/access.lua
@@ -17,6 +17,8 @@ local type = type
 local string_find = string.find
 local pcall = pcall
 
+local responses = require "kong.tools.responses"
+
 local _M = {}
 
 function _M.execute(conf)
@@ -24,10 +26,35 @@ function _M.execute(conf)
   math.randomseed(os.time())
   math.random()
   if math.random() < jitter_rate then
-    ngx.status = ngx.HTTP_REQUEST_TIMEOUT
-    ngx.header["Content-Type"] = "application/json; charset=utf-8";
-    ngx.say("timeout")
-    return ngx.exit(ngx.HTTP_REQUEST_TIMEOUT)
+
+    --local ok, err
+    --local parsed_url = parse_url(conf.http_endpoint)
+    --local host = parsed_url.host
+    --local port = tonumber(parsed_url.port)
+    --
+    --local sock = ngx.socket.tcp()
+    --sock:settimeout(conf.timeout)
+    --
+    --ok, err = sock:connect(host, port)
+    --if not ok then
+    --  ngx.log(ngx.ERR, name .. "failed to connect to " .. host .. ":" .. tostring(port) .. ": ", err)
+    --  return responses.send(ngx.HTTP_REQUEST_TIMEOUT, err)
+    --end
+    --
+    --if parsed_url.scheme == HTTPS then
+    --  local _, err = sock:sslhandshake(true, host, false)
+    --  if err then
+    --    ngx.log(ngx.ERR, name .. "failed to do SSL handshake with " .. host .. ":" .. tostring(port) .. ": ", err)
+    --  end
+    --end
+    --
+    --ok, err = sock:send(generate_post_payload(conf.method, conf.content_type, parsed_url, body))
+    --if not ok then
+    --  ngx.log(ngx.ERR, name .. "failed to send data to " .. host .. ":" .. tostring(port) .. ": ", err)
+    --end
+    ngx.ctx.balancer_address.connect_timeout = 1
+    ngx.ctx.balancer_address.send_timeout = 1
+    ngx.ctx.balancer_address.read_timeout = 1
   end
 end
 
