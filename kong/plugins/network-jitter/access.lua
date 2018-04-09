30ce868639ebd513fe6b0156e15eca4f75c09c63
alex1986 <alex19861108@live.cn>
2018-04-09 11:00:18 +0800

    add module: network-jitter

--- kong/plugins/network-jitter/access.lua
+++ kong/plugins/network-jitter/access.lua
@@ -26,10 +26,9 @@ function _M.execute(conf)
   math.random()
   ngx.log(ngx.ERR, "===========")
   ngx.log(ngx.ERR, conf.connect_timeout.rate)
-  ngx.log(ngx.ERR, math.random())
-  ngx.log(ngx.ERR, math.random() < conf.connect_timeout.rate )
   ngx.log(ngx.ERR, "===========")
   if conf.connect_timeout and math.random() < conf.connect_timeout.rate then
+    ngx.log(ngx.ERR, "**************")
     ngx.ctx.balancer_address.connect_timeout = 1
   end
   --if conf.send_timeout and math.random() < conf.send_timeout.rate then
--- kong/plugins/network-jitter/schema.lua
+++ kong/plugins/network-jitter/schema.lua
@@ -46,7 +46,6 @@ return {
     --},
   },
   self_check = function(schema, plugin_t, dao, is_update)
-    ngx.log(ngx.ERR, plugin_t)
     if plugin_t.connect_timeout then
       if plugin_t.connect_timeout.rate < 0 or plugin_t.connect_timeout.rate > 1 then
         return false, Errors.schema("connect_timeout_rate must be between 0 .. 1")
