661ad8e778eceee16a9ba4fc73ca05cfa2465271
alex1986 <alex19861108@live.cn>
2018-04-09 16:37:28 +0800

    add module: network-jitter

--- kong/plugins/network-jitter/schema.lua
+++ kong/plugins/network-jitter/schema.lua
@@ -1,3 +1,4 @@
+local Errors = require "kong.dao.errors"
 
 return {
   fields = {
@@ -45,25 +46,26 @@ return {
       }
     },
   },
-  self_check = function(schema, plugin_t, dao, is_update)
+
+  self_check = function(schema, plugin_t, dao, is_updating)
     if plugin_t.connect_timeout ~= nil then
       if plugin_t.connect_timeout.rate < 0 or plugin_t.connect_timeout.rate > 1 then
-        return false, Errors.schema("connect_timeout_rate must be between 0 .. 1")
+        return false, Errors.schema("connect_timeout。rate must be between 0 .. 1")
       end
     end
     if plugin_t.send_timeout ~= nil then
       if plugin_t.send_timeout.rate < 0 or plugin_t.send_timeout.rate > 1 then
-        return false, Errors.schema("send_timeout_rate must be between 0 .. 1")
+        return false, Errors.schema("send_timeout.rate must be between 0 .. 1")
       end
     end
     if plugin_t.read_timeout ~= nil then
       if plugin_t.read_timeout.rate < 0 or plugin_t.read_timeout.rate > 1 then
-        return false, Errors.schema("read_timeout_rate must be between 0 .. 1")
+        return false, Errors.schema("read_timeout.rate must be between 0 .. 1")
       end
     end
     if plugin_t.upstream_disconnect ~= nil then
       if plugin_t.upstream_disconnect.rate < 0 or plugin_t.upstream_disconnect.rate > 1 then
-        return false, Errors.schema("upstream_disconnect_rate must be between 0 .. 1")
+        return false, Errors.schema("upstream_disconnect.rate must be between 0 .. 1")
       end
     end
     if plugin_t.request_termination ~= nil then
